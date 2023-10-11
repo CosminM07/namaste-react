01c29904c7498412848c277f0b635832b2030b95
CosminM07 <cosmin.munteanu@vitals.co>
2023-10-12 02:38:13 +0300

    Episode 5

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,70 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState } from "react";
+import resList from "../utils/mockData";
 
 const Body = () => {
+  // Local State Variable - Super powerful variable
+  const [listOfRestaurants, setListOfRestraunt] = useState(resList);
 
-    // Local State Variable - Super powerful variable
-    const [listOfRestaurants, setListOfRestaurants] = useState([
-        {
-        type: "restaurant",
-        data: {
-            id: "334475",
-            name: "KFC",
-            cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-            cuisines: ["Burgers", "Biryani", "American"],
-            costForTwo: 40000,
-            deliveryTime: 36,
-            avgRating: "3.8"
-        }
-        },
-        {
-            type: "restaurant",
-            data: {
-                id: "334476",
-                name: "Dominos",
-                cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-                cuisines: ["Burgers", "Biryani", "American"],
-                costForTwo: 40000,
-                deliveryTime: 36,
-                avgRating: "4.5"
-            }
-        },
-        {
-            type: "restaurant",
-            data: {
-                id: "334477",
-                name: "MCdonalts",
-                cloudinaryImageId: "bdcd233971b7c81bf77e1fa4471280eb",
-                cuisines: ["Burgers", "Biryani", "American"],
-                costForTwo: 40000,
-                deliveryTime: 36,
-                avgRating: "4.1"
-            }
-        },
-    ]);
-    console.log(listOfRestaurants)
-   
-
-    return (
-        <div className="body">
-            <div className="filter">
-                <button 
-                    className="filter-btn" 
-                    onClick={() => {
-                        // Filtler logic her
-                        const filtredList = listOfRestaurants.filter(res => res.data.avgRating > 4)
-                        console.log(filtredList)
-                        setListOfRestaurants(filtredList)
-                    }}
-                >Top Rated Restaurants</button>
-            </div>
-            <div className="res-container">
-                {
-                    listOfRestaurants.map(restaurant => <RestaurantCard key={restaurant.data.id} resData={restaurant} />)
-                }
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestraunt(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Body;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,5 @@
 export const CDN_URL =
-  "https://plus.unsplash.com/premium_photo-1663858367001-89e5c92d1e0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=715&q=80";
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const LOGO_URL =
   "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
-
-export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
