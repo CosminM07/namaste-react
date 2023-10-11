b8f664eb4821bcfa154a5e8107d774e99407f634
CosminM07 <cosmin.munteanu@vitals.co>
2023-10-12 02:23:39 +0300

    Episode 5

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const CDN_URL =
+  "https://plus.unsplash.com/premium_photo-1663858367001-89e5c92d1e0e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=715&q=80";
+
+export const LOGO_URL =
+  "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
