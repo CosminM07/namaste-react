97d9b1c4dd59e72e4761d7ed58392da2d7e267d0
CosminM07 <cosmin.munteanu@vitals.co>
2024-01-12 21:37:58 +0200

    Episode 02

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
-  "Hello World from React!"
+  "Hello World from React s!"
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
