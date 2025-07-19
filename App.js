35e1db5347407f4323df151fee35356c5636bf3f
CosminM07 <cosmin.munteanu@vitals.co>
2025-07-19 19:37:38 +0300

    episode 03

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const TitleComponent = () => {
-  return <h1>Namaste React</h1>;
-};
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX 🚀
+  </h1>
+);
 
-const BodyComponent = () => {
-  return <div>Asta este body</div>;
-};
-
-const HeadingComponent = () => {
-  return (
-    <div>
-      <TitleComponent />
-      <BodyComponent />
+// React Fragment - behaves like an empty tag
+const HeadingComponent = () => (
+  <>
+    <div id="container">
+      <Title />
+      <h1 className="heaidng">Namaste React Fucntional Component</h1>
     </div>
-  );
-};
+    <div id="container-2"></div>
+  </>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
